b99ee86c0fc341597a55546ea1b1a7a169fc9881
francist <65456051+francistops@users.noreply.github.com>
2025-11-25 15:36:12 -0500

    messing around with db connection to get it working

--- src/app/helper/db_connect.php
+++ src/app/helper/db_connect.php
@@ -1,11 +1,9 @@
 <?php
-// Toujours partir du répertoire racine du projet
-require_once __DIR__ . '/../config.php';
+echo __DIR__ . '/../../config.php';
+require_once __DIR__ . '/../../config.php';
 
-// Connexion MySQL
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-// Vérifier la connexion
 if ($conn->connect_error) {
     die("Erreur de connexion à la base de données : " . $conn->connect_error);
 }
