5ee5331a81cf87f5e4770cb45527fdd946c0e819
ThomasRivard <thomas.s.rivard@gmail.com>
2025-12-02 16:47:59 -0200

    update mvc index

--- src/app/views/home/index.php
+++ src/app/views/home/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php  
 require_once(CONTROLLERS_PATH . '/Films.php');
 include_once(VIEWS_PATH . '/partials/header.php');
 $films = get_films();
@@ -6,13 +6,14 @@ $films = get_films();
 
 <h2>Liste des films</h2>
 <ul>
-    <?php while ($film = $films->fetch_assoc()): ?>
-        <li>
-            <a href="<?php echo htmlspecialchars('index.php?action=films&id=' . $film['id']); ?>">
-                <?php echo htmlspecialchars($film['titre']) . ' (' . htmlspecialchars($film['annee_sortie']) . ')'; ?>
-            </a>
-        </li>
-    <?php endwhile; ?>
+    <?php 
+    foreach($films as $film){
+         echo'<li><a href="film.php?id="'.$film["id"].'">'.$film["titre"].'</a></li>';
+
+    }
+    ?>     
 </ul>
 
-<?php include_once(VIEWS_PATH . '/partials/footer.php'); ?>
+
+<?php include_once (VIEWS_PATH . '/partials/footer.php'); ?>
+
