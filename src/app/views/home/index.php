950ae221c217b8a645274e2680e7d2537fb9d63b
francist <65456051+francistops@users.noreply.github.com>
2025-12-02 16:52:09 -0500

    remove db call in index.php

--- src/app/views/home/index.php
+++ src/app/views/home/index.php
@@ -6,13 +6,13 @@ $films = get_films();
 
 <h2>Liste des films</h2>
 <ul>
-    <?php while ($film = $films->fetch_assoc()): ?>
+    <?php foreach ($films as $film): ?>
         <li>
             <a href="<?php echo htmlspecialchars('index.php?action=films&id=' . $film['id']); ?>">
                 <?php echo htmlspecialchars($film['titre']) . ' (' . htmlspecialchars($film['annee_sortie']) . ')'; ?>
             </a>
         </li>
-    <?php endwhile; ?>
+    <?php endforeach; ?>
 </ul>
 
 <?php include_once(VIEWS_PATH . '/partials/footer.php'); ?>
